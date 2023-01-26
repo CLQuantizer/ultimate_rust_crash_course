d88ff7c66293dfaa3831e57278ecb731fc47bb5c
ezio <ezio.xie@binance.com>
2023-01-26 13:15:48 +0000

    not compiling

--- erros_demo/src/main.rs
+++ erros_demo/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     let s1 = String::from("Kaggle");
-    let s1 = takes_and_returns(s1);
+    borrows(&s1);
     println!("I am {}", s1);
+
 }
 
-fn takes_and_returns(prey: String) -> String{
-    println!("I lowkey possess the soul of {}", prey);
-    prey
+fn borrows(prey: &String){
+    prey.push_str(" is so dead");
 }
